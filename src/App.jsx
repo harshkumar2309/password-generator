be0e683c73000715c117cd3bc2e4f5be7d68be6b
Harsh Kumar <yadavharsh1201@gmail.com>
2025-07-14 23:00:55 +0530

    add highlight fetaure after copying

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
   }, [length, numAllowed, charAllowed, setPassword])
 
   const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
     window.navigator.clipboard.writeText(password)
   }, [password])
 
